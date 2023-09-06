b1ccca90767c4dad0e17c4a76d70f4fcb9715fc8
DIDICHAA <jenny615@dongguk.edu>
2023-09-07 02:47:05 +0900

    Refactor: project folder urls

--- usher/urls.py
+++ usher/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    #path('dj-rest-auth/', include('dj_rest_auth.urls')),
     #path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/', include('user.urls')),
     path('api/', include('community.urls')),
