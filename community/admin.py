9cf2473ed54d120c6a4c473e933ee0d548c4b0ae
DIDICHAA <jenny615@dongguk.edu>
2023-09-07 14:09:26 +0900

    Feat: ComPost Admin add

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ComPost
 
-# Register your models here.
+class ComPostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'writer', 'created_at', 'updated_at', 'category', 'views',)
+    search_fields = ('title', 'writer__nickname',)
+    inlines = [ComPostMediaInline]
+
+admin.site.register(ComPost, ComPostAdmin)
